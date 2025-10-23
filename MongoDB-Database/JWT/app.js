fea39b1d6ae97bc207edb7b4f68563e4b325d66e
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-10-23 21:09:22 +0530

    completed JWT in MongoDB

--- MongoDB-Database/JWT/app.js
+++ MongoDB-Database/JWT/app.js
@@ -10,11 +10,12 @@ app.get('/', function (req, res) {
     var token = jwt.sign({ email: 'node@gmail.com' }, 'Secret');  //This 'Secret' string is very sensitive, usually it is not a plain text
     res.cookie("token", token)
     console.log(token)
+    
     res.send("Backend is running");
 })
 
 app.get('/read', function (req, res) {
-    let data = jwt.verify(req.cookies.token, "Secret")
+    let data = jwt.verify(req.cookies.token, "Secret")  // This Data variable stored the payload data but in decrypted string (plain text)
     console.log(data)
 })
 
